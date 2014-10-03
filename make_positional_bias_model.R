1de3afc51c4d8fd98739bd001b1626db4017f465
Alyssa Frazee <alyssa.frazee@gmail.com>
2014-10-03 14:40:11 -0400

    sequence "transcript percent" starting at 1, not 0 (BECAUSE R)

--- make_positional_bias_model.R
+++ make_positional_bias_model.R
@@ -3,7 +3,7 @@
 ### RNA fragmentation model:
 rnaf = read.csv('rnaf_pos_bias.csv', header=FALSE, colClasses=c('numeric', 'numeric'))
 names(rnaf) = c('pospct', 'prob')
-rnaf$pospct = seq(0, 0.99, by=0.01) #x-axis should be calibrated from 0-99%, but it was hard to click exactly :) 
+rnaf$pospct = seq(0.01, 1, by=0.01) #x-axis should be calibrated from 0-99%, but it was hard to click exactly :) 
 rnaf$prob[rnaf$prob < 0] = 0 #minor adjustment to y-axis calibration
 # make sure probabilities sum to 1:
 rnaf$prob = rnaf$prob/sum(rnaf$prob) #it was super close anyway, but let's be careful.
@@ -13,7 +13,7 @@ save(rnaf, file='data/rnaf.rda', compress='xz')
 ### cDNA fragmentation model:
 cdnaf = read.csv('cdna_pos_bias.csv', header=FALSE, colClasses=c('numeric', 'numeric'))
 names(cdnaf) = c('pospct', 'prob')
-cdnaf$pospct = seq(0, 0.99, by=0.01)
+cdnaf$pospct = seq(0.01, 1, by=0.01)
 cdnaf$prob[cdnaf$prob < 0] = 0
 cdnaf$prob = cdnaf$prob/sum(cdnaf$prob)
 
