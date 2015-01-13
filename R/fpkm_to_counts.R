c7247cb598a51892eb0d4247010d03c086f5c0de
Alyssa Frazee <alyssa.frazee@gmail.com>
2015-01-13 10:18:51 -0500

    update fpkm_to_counts to take a matrix

--- R/fpkm_to_counts.R
+++ R/fpkm_to_counts.R
@@ -1,14 +1,23 @@
 #' Turn FPKMs from a ballgown object into estimated counts for transcripts
 #'
 #' @param bg ballgown object created from real RNA-seq dataset
-#' @param threshold only estimate parameters from transcripts with mean FPKM 
-#'   measurements larger than \code{threshold}
+#' @param mat matrix of isoform-level FPKMs from which to derive counts. Rows
+#'   should represent transcripts and columns should represent counts. Provide
+#'   exactly one of \code{bg} or \code{mat}. 
+#' @param tlengths if using \code{mat} instead of \code{bg}, vector of
+#'   transcript lengths. Entries correspond to the rows of \code{mat}. Lengths
+#'   should only count the nucleotides within transcripts' exons. 
 #' @param mean_rps This should be the number of reads per sample in total for
-#'   use in backing out the FPKM calculations
+#'   use in backing out the FPKM calculations.
+#' @param threshold only estimate parameters from transcripts with mean FPKM 
+#'   measurements at least as large as \code{threshold}.
 #' 
 #' @return A matrix of counts with the same number of rows and columns as the
 #'   ballgown object
 #' 
+#' @details If transcripts/exons are represented by \code{GRanges} or
+#'   \code{GRangesList} objects, the \code{width} function is really useful
+#'   in calculating transcript lengths.
 #' @export
 #' @author Jeff Leek
 #' @examples
@@ -18,12 +27,18 @@
 #' 
 #' 
 
-fpkm_to_counts = function(bg, mean_rps=100e6, threshold=0){
-    tmeas = as.matrix(ballgown::texpr(bg, "FPKM"))
-    trowm = rowMeans(tmeas)
-    index1 = which(trowm > threshold)
-    tlengths = sapply(width(ballgown::structure(bg)$trans[index1]), sum)
+fpkm_to_counts = function(bg=NULL, mat=NULL, tlengths=NULL, mean_rps=100e6, 
+    threshold=0){
+    if(is.null(mat)){
+        tmeas = as.matrix(ballgown::texpr(bg, 'FPKM'))
+        tlengths = sapply(width(ballgown::structure(bg)$trans), sum)
+    }else{
+        tmeas = mat
+        stopifnot(!is.null(tlengths))
+    }
+    index1 = which(rowMeans(tmeas) >= threshold)
+    tlengths = tlengths[index1]
     counts = tlengths*tmeas[index1,]/1000
     counts = round(counts*mean_rps/1e6)
-    return(counts)
+    return(counts)    
 }
