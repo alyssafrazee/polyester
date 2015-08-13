cd8b6fbad4ae57005f682ed100e32b015897f5f1
John <john.casement@ncl.ac.uk>
2015-08-13 14:19:54 +0100

    Using Alyssa's original get_reads (unchanged)

--- R/get_reads.R
+++ R/get_reads.R
@@ -19,63 +19,54 @@
 #'   srPhiX174_reads = get_reads(srPhiX174, readlen=15, paired=FALSE)
 #'   srPhiX174_reads  
 #'   # set of single-end, 15bp reads, treating srPhiX174 as the fragments
-get_reads = function(tFrags, readlen, paired=TRUE, library_type){
+get_reads = function(tFrags, readlen, paired=TRUE){
   
-    # when fragments are shorter than reads:
-    isShort = (width(tFrags) <= readlen)
-    isLong = !isShort
-      
-    if(paired) {
-      
-        if(sum(isShort) > 0){
-            x = tFrags[isShort] # left mate
-            names(x) = paste0(seq(along=x), "a")
-            # if unstranded, then reverse complement
-            if(library_type == "unstranded") {
-              rr = reverseComplement(x) # right mate              
-            } else {
-              # if firststrand or secondstrand, don't reverse complement
-              rr = x # right mate                                     
-            }
-            names(rr) = paste0(seq(along=x), "b")
-            out = c(x,rr)
-            outInds = rep(1:length(x), each=2)
-            outInds[seq(2, length(outInds), by=2)] = (1:length(x))+length(x)
-            outShort = out[outInds] # puts pairs of reads next to each other
-            names(outShort) = paste0(rep(names(tFrags)[isShort], each=2))
-        }
+  # when fragments are shorter than reads:
+  isShort = (width(tFrags) <= readlen)
+  isLong = !isShort
+  
+  if(paired) {
     
-        if(sum(isLong) > 0){
-            x = tFrags[isLong]
-            lr = subseq(x, start=1, end=readlen) # left mate
-            names(lr) = paste0(seq(along=x), "a")
-            rr = subseq(x, start=(width(x)-readlen+1), end=width(x))
-            # if unstranded, then reverse complement
-            if(library_type == "unstranded") {
-              rr = reverseComplement(rr) # right mate              
-            }
-            names(rr) = paste0(seq(along=x), "b")
-            out = c(lr, rr)
-            outInds = rep(1:length(lr), each=2)
-            outInds[seq(2, length(outInds), by=2)] = (1:length(lr))+length(lr)
-            outLong = out[outInds] # puts pairs of reads next to each other
-            names(outLong) = paste0(rep(names(tFrags)[isLong], each=2))    
-        }
-      
-        if(sum(isLong) > 0 & sum(isShort) > 0) {
-            theReads = c(outLong, outShort)
-        } else if(sum(isLong) > 0) {
-            theReads = outLong
-        } else {
-            theReads = outShort
-        }
-      
-        return(theReads)
-    
-    } else { #single end
-      theReads = tFrags
-      theReads[isLong] = subseq(tFrags[isLong], start=1, end=readlen)
-      return(theReads)
+    if(sum(isShort) > 0){
+      x = tFrags[isShort] # left mate
+      names(x) = paste0(seq(along=x), "a")
+      rc = reverseComplement(x) # right mate
+      names(rc) = paste0(seq(along=x), "b")
+      out = c(x,rc)
+      outInds = rep(1:length(x), each=2)
+      outInds[seq(2, length(outInds), by=2)] = (1:length(x))+length(x)
+      outShort = out[outInds] # puts pairs of reads next to each other
+      names(outShort) = paste0(rep(names(tFrags)[isShort], each=2))
     }
     
+    if(sum(isLong) > 0){
+      x = tFrags[isLong]
+      lr = subseq(x, start=1, end=readlen) # left mate
+      names(lr) = paste0(seq(along=x), "a")
+      rr = subseq(x, start=(width(x)-readlen+1), end=width(x))
+      rr = reverseComplement(rr) # right mate
+      names(rr) = paste0(seq(along=x), "b")
+      out = c(lr, rr)
+      outInds = rep(1:length(lr), each=2)
+      outInds[seq(2, length(outInds), by=2)] = (1:length(lr))+length(lr)
+      outLong = out[outInds] # puts pairs of reads next to each other
+      names(outLong) = paste0(rep(names(tFrags)[isLong], each=2))    
+    }
+    
+    if(sum(isLong) > 0 & sum(isShort) > 0) {
+      theReads = c(outLong, outShort)
+    } else if(sum(isLong) > 0) {
+      theReads = outLong
+    } else {
+      theReads = outShort
+    }
+    
+    return(theReads)
+    
+  } else { #single end
+    theReads = tFrags
+    theReads[isLong] = subseq(tFrags[isLong], start=1, end=readlen)
+    return(theReads)
+  }
+  
 }
