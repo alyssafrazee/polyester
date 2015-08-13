5cb603d700daf3af909e7fffcbaec01d090b669e
John <john.casement@ncl.ac.uk>
2015-08-13 15:49:50 +0100

    Modified reverse_complement

--- R/reverse_complement.R
+++ R/reverse_complement.R
@@ -16,11 +16,14 @@ reverse_complement = function(tObj, library_type, strand_error_rate, seed=NULL){
   if(!is.null(seed)) set.seed(seed)
   
   if(library_type == 'firststrand'){
+    cat("I'm a FIRSTSTRAND simulation...\n")
     strand = sample(c(1,0), length(tObj), replace=TRUE, prob = c(strand_error_rate, 1 - strand_error_rate))
   }else if(library_type == 'secondstrand'){
+    cat("I'm a SECONDSTRAND simulation...\n")
     strand = sample(c(0,1), length(tObj), replace=TRUE, prob = c(strand_error_rate, 1 - strand_error_rate))
   } else {
     # default unstranded option
+    cat("I'm an UNSTRANDED simulation...\n")
     strand = sample(c(0,1), length(tObj), replace=TRUE)
   }
   tObj[strand==0] = reverseComplement(tObj[strand==0])
