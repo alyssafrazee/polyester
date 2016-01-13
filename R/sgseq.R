cd844b0bed0f3c59b4f4b56570f77ded2811252d
Jack Fu <jmfu@jhsph.edu>
2016-01-13 10:11:47 -0500

    Added function to report coverage of generated transcripts

--- R/sgseq.R
+++ R/sgseq.R
@@ -1,7 +1,14 @@
 ## internal sequencing function
 
-sgseq = function(readmat, transcripts, paired, outdir, extras){
-
+sgseq = function(readmat, transcripts, paired, outdir, extras, reportCoverage=F){
+      #report theoretically perfect coverage if reportCoverage=T, will write a file
+      if(reportCoverage==T){
+            templates = unique(transcripts)
+            coverage_matrices = list()
+            for(i in 1:length(templates)){coverage_matrices = c(coverage_matrices, list(matrix(0, ncol=dim(readmat)[2], width(templates)[i])))}
+            names(coverage_matrices) = names(templates)
+      }
+      
   for(i in seq_len(ncol(readmat))) {
     tObj = rep(transcripts, times=readmat[,i])
     iterations = ceiling(length(tObj) / 1e6L)
@@ -10,13 +17,27 @@ sgseq = function(readmat, transcripts, paired, outdir, extras){
       tSubset = tObj[offset:min(offset+1e6L, length(tObj))]
       tFrags = generate_fragments(tSubset, extras$fraglen, extras$fragsd,
                                   extras$readlen, extras$distr, extras$custdens,
-                                  extras$bias)
+                                  extras$bias)      
       #reverse_complement some of those fragments
       rctFrags = reverse_complement(tFrags)
 
       #get reads from fragments
       reads = get_reads(rctFrags, extras$readlen, paired)
 
+      if(reportCoverage==T){
+            read_info = unique(names(reads))
+            read_info_split = strsplit(read_info, ";mate1:|;mate2:")
+            read_info_matrix = matrix(unlist(read_info_split), ncol=3, byrow=T)
+            for(j in 1:dim(read_info_matrix)[1]){
+                  read = read_info_matrix[j,]
+                  target = which(names(coverage_matrices)==read[1])
+                  coords1 = unlist(str_split(read[2], "-"))
+                  coords2 = unlist(str_split(read[3], "-"))
+                  coverage_matrices[[target]][coords1[1]:coords1[2],i]=coverage_matrices[[target]][coords1[1]:coords1[2],i]+1
+                  coverage_matrices[[target]][coords2[1]:coords2[2],i]=coverage_matrices[[target]][coords2[1]:coords2[2],i]+1
+            }
+      }
+      
       #add sequencing error
       if(extras$error_model == 'uniform'){
           errReads = add_error(reads, extras$error_rate)
@@ -33,4 +54,6 @@ sgseq = function(readmat, transcripts, paired, outdir, extras){
       offset = offset + 1e6L
     }
   }
+  save(coverage_matrices, file=paste0(outdir, '/sample_coverages.rda') )
+  
 }
